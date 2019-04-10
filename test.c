8b9e8c3391546b8372acda4043e053cab3929dcc
Swethakadam <49471132+Swethakadam@users.noreply.github.com>
2019-04-10 15:44:23 +0530

    Create test.c

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("enter the num\n");
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+        printf("even number\n");
+    }
+    else
+        printf("odd number\n");
+    return 0;
+    }
